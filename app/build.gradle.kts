a982650b2fe4b1e9b529e377b6358eb052f9f3ba
Andrew Emmanuel A. Abarientos <aeabarientos@gbox.adnu.edu.ph>
2025-06-27 11:19:50 +0800

    fry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,5 +103,7 @@ dependencies {
     implementation ("io.insert-koin:koin-androidx-compose:3.5.0")
     implementation ("io.insert-koin:koin-core:3.5.0")
     implementation ("com.auth0:java-jwt:4.4.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
 }
